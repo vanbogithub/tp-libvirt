65f3abd14ab973a2fed5b9fc0b445409f5f737c6
Wei Shi <wshi@redhat.com>
2014-02-21 14:45:38 +0800

    Add run_mode to guestfish_block_dev

--- libguestfs/tests/guestfish_block_dev.py
+++ libguestfs/tests/guestfish_block_dev.py
@@ -1,5 +1,10 @@
 from autotest.client.shared import error, utils
-from virttest import utils_test
+from virttest import utils_test, utils_misc, data_dir
+from virttest.tests import unattended_install
+import logging
+import shutil
+import os
+
 
 def test_blockdev_flushbufs(vm, params):
     """
@@ -21,6 +26,48 @@ def test_blockdev_flushbufs(vm, params):
         raise error.TestFail(createo)
     gf.close_session()
 
+    gf = utils_test.libguestfs.GuestfishTools(params)
+    image_path = params.get("image_path")
+    gf.add_drive(image_path)
+    gf.run()
+    pv_name = params.get("pv_name")
+    gf_result = gf.blockdev_flushbufs(pv_name)
+    logging.debug(gf_result)
+    if gf_result.exit_status:
+        gf.close_session()
+        raise error.TestFail("blockdev_flushbufs failed.")
+    logging.info("Get readonly status successfully.")
+    gf.close_session()
+
+    add_ref = params.get("guestfs_add_ref", "disk")
+    run_mode = params.get("guestfs_run_mode", "interactive")
+
+    if run_mode == "interactive":
+        if add_ref == "disk":
+            pass
+        elif add_ref == "remote":
+            pass
+        else:
+            raise error.TestFail("Invalid parameter guestfs_add_ref = %s" % add_ref)
+    elif run_mode == "remote":
+        pass
+    else:
+        raise error.TestFail("Invalid parameter guestfs_run_mode = %s" % run_mode)
+
+def image_copy(params):
+    image = '%s.%s' % (params['image_name'], params['image_format'])
+    image_name = os.path.basename(image)
+    src = params.get('images_good')
+    dst = '%s/%s' % (data_dir.get_data_dir(), image)
+    mount_point = params.get("dst_dir")
+
+    utils_misc.mount(src, mount_point, "nfs", perm="ro")
+    image_src = utils_misc.get_path(mount_point, image_name)
+    logging.debug("Copying image %s to %s", image_src, dst)
+    shutil.copy(image_src, dst)
+    utils_misc.umount(src, mount_point, "nfs")
+
+
 def run(test, params, env):
     """
     Test of built-in block_dev related commands in guestfish.
@@ -38,11 +85,16 @@ def run(test, params, env):
     vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
 
+    if vm.is_alive():
+        vm.destroy()
+
     operation = params.get("guestfish_function")
     testcase = globals()["test_%s" % operation]
     partition_types = params.get("partition_types")
     fs_types = params.get("fs_types")
     image_formats = params.get("image_formats")
+
+#    image_copy(params)
 
     for image_format in image_formats.split(" "):
         params["image_format"] = image_format
@@ -51,3 +103,5 @@ def run(test, params, env):
             for fs_type in fs_types.split(" "):
                 params["fs_type"] = fs_type
                 testcase(vm, params)
+
+
